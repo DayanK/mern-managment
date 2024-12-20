689a09a7bfc9f97d4a333eb4926803aab1e28114
Maxim Kemajou <mckemajou@live.fr>
2024-12-20 19:51:05 +0100

    init project

---
+++ server/sampleData.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'Development of a fully functional eCommerce platform with features including product listings, shopping cart, payment gateway integration, and user account management.',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Dating App',
+      description:
+        'Creation of a dating application with user profiles, matching algorithms, real-time chat, and social media integration.',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'Enhancing the SEO of an existing website to improve search engine rankings, increase organic traffic, and optimize on-page elements.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'Development of a high-fidelity design prototype for a mobile application, including user interface and user experience elements.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'Building an online auction platform with real-time bidding, user authentication, and payment processing capabilities.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'Tony Stark',
+      email: 'tony.stark@starkindustries.com',
+      phone: '343-567-4333',
+    },
+    {
+      id: '2',
+      name: 'Natasha Romanova',
+      email: 'natasha.romanova@shield.gov',
+      phone: '223-567-3322',
+    },
+    {
+      id: '3',
+      name: 'Thor Odinson',
+      email: 'thor.odinson@asgard.com',
+      phone: '324-331-4333',
+    },
+    {
+      id: '4',
+      name: 'Steve Rogers',
+      email: 'steve.rogers@avengers.com',
+      phone: '344-562-6787',
+    },
+    {
+      id: '5',
+      name: 'Bruce Banner',
+      email: 'bruce.banner@avengers.com',
+      phone: '321-468-8887',
+    },
+  ];
+  
+  module.exports = { projects, clients };
